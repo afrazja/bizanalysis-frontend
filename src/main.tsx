d7d71fd5c75a07d4a9e975803b9afa66fa9d8669
Afraz Javanmardi <45622413+afrazja@users.noreply.github.com>
2025-08-12 21:23:56 -0700

    feat(frontend): add routes for readonly snapshot view

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App'
+import ViewSnapshot from './pages/ViewSnapshot'
 import './styles.css'
+
+const router = createBrowserRouter([
+  { path: '/', element: <App /> },
+  { path: '/view/:id', element: <ViewSnapshot /> },
+])
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
